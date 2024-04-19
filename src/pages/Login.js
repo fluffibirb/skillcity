7295f8e4c5dfaa48ee7e67427a05dfeaeac51219
fluffibirb <jessica_tam@hotmail.co.uk>
2024-04-19 15:46:03 +0100

    login files

---
+++ src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form action="">
+        <div>
+          <label htmlFor="">Email: </label>
+        </div>
+        <div>
+          <label htmlFor="">Password: </label>
+        </div>
+        <button></button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
