ab2202ab45a1a766d4183177005360a951d68ce9
fluffibirb <jessica_tam@hotmail.co.uk>
2024-04-22 16:54:16 +0100

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import HolidayNotification from "./pages/HolidayNotification";
+import RequestHoliday from "./pages/RequestHoliday";
 
-function App() {
+// Define your main App component
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation */}
+        <nav>
+          <ul>
+            {/* Link components for navigation */}
+            <li>
+              <Link to="/">
+                <Profile></Profile>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" exact component={Profile} />
+          <Route path="/holiday" component={RequestHoliday} />
+          <Route path="/log" component={HolidayNotification} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/HolidayNotification.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HolidayNotification = () => {
+  return <div></div>;
+};
+
+export default HolidayNotification;
